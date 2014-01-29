71dbf554849d64720b261b1c355564e29ee8815b
oneusha <oneusha@Mac-mini.local>
2014-01-29 17:38:58 +0400

    Initial commit
    There is a simple class for played field and player class. Players can
    put a symbol to the field.

---
+++ src/Field.java
@@ -0,0 +1,61 @@
+import com.sun.swing.internal.plaf.synth.resources.synth_pt_BR;
+
+public class Field {
+
+    private static final int MIN_FIELD_SIZE = 1;
+    private static final int MAX_FIELD_SIZE = 10;
+    private static final int DEFAULT_FIELD_SIZE = 3;
+    private static final char EMPTY_CELL_SYMBOL = ' ';
+
+    private final int fieldSize;
+    private final char[][] field;
+
+    public Field() {
+        this(DEFAULT_FIELD_SIZE);
+    }
+
+    public Field(int fieldSize) {
+        if ((fieldSize > MIN_FIELD_SIZE) && (fieldSize < MAX_FIELD_SIZE)) {
+            this.fieldSize = fieldSize;
+        } else {
+            this.fieldSize = DEFAULT_FIELD_SIZE;
+        }
+        field = new char[fieldSize][fieldSize];
+    }
+
+    public void clearField() {
+        for (int i = 0; i < fieldSize; i++) {
+            clearLine(i);
+        }
+    }
+
+    private void clearLine(int lineNumber) {
+        for (int i = 0; i < fieldSize; i++) {
+            field[lineNumber][i] = EMPTY_CELL_SYMBOL;
+        }
+    }
+
+    public void showField() {
+        for (int i = 0; i < fieldSize; i++) {
+            showLine(i);
+            System.out.println();
+        }
+        System.out.println();
+    }
+
+    private void showLine(int lineNumber) {
+        for (int i = 0; i < fieldSize; i++) {
+            showCell(lineNumber, i);
+        }
+    }
+
+    private void showCell(int i, int j) {
+        System.out.print("[" + field[i][j] + "]");
+    }
+
+    public void drowSymbol(Player player, int i, int j) {
+        System.out.println(player.getName() + ":");
+        field[i][j] = player.getSymbol();
+        showField();
+    }
+}
---
+++ src/Player.java
@@ -0,0 +1,26 @@
+public class Player {
+
+    private String name;
+
+    private final int id;
+
+    private char symbol;
+
+    public Player(String name, int id, char symbol) {
+        this.name = name;
+        this.id = id;
+        this.symbol = symbol;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public char getSymbol() {
+        return symbol;
+    }
+}
